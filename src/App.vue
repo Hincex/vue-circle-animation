638d428c9fccc3c29a92428d7a0270269c4ad0b0
Vincehx97 <Vincehx97@Gmail.com>
2019-06-27 15:22:27 +0800

    文件上传

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <circle-ani />
+  </div>
+</template>
+
+<script>
+// import CircleAnimation from './components/CircleAnimation.vue'
+import CircleAni from './components/Circle.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CircleAni
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Circle.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <div class="circle">
+        <canvas-circle 
+        v-for="(item,index) in cicleList" 
+        :key="index" 
+        :percent="item.ratio"
+        :unit="'分'"
+        :id="index"
+        :width=200
+        :height=200
+        :ringR=17
+        :speed=0.4>
+        </canvas-circle>
+    </div>
+</template>
+<script>
+import canvasCircle from './canvasCircle'
+export default {
+    name: 'CircleAni',
+    components: {
+    canvasCircle
+  },
+  data () {
+    return {
+      cicleList: [{
+        ratio: 35
+      },{
+        ratio: 65
+      },{
+        ratio: 85
+      }]
+    }
+  }
+}
+</script>
